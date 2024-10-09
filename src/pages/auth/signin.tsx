15615dcd75f3c799b3741f3a62f47d77bd9250a9
sarraTouhami <sarra.touhami@esprit.tn>
2024-10-09 23:28:27 +0100

    signin with google

---
+++ src/pages/auth/signin.tsx
@@ -0,0 +1,12 @@
+import { signIn } from 'next-auth/react';
+
+const SignIn = () => {
+  return (
+    <div>
+      <h1>Sign In</h1>
+      <button onClick={() => signIn('google')}>Sign in with Google</button>
+    </div>
+  );
+};
+
+export default SignIn;
